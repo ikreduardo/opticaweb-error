793f42f7a1d017a07773248a0bc1340825de2f0e
Jose Luis Martinez <jmartinez@zarotransportation.com>
2020-07-29 14:58:17 -0500

    func

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  endPoint = environment.endpoint
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
+
+  login() {
+    //return this.http.post()
+  }
+
+  logout() {
+
+  }
+
+  setToken() {
+
+  }
 }
