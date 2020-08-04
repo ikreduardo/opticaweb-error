1d580def77bd4f26ec20576ea71b7c15143d9e86
Jose Luis Martinez <jmartinez@zarotransportation.com>
2020-08-04 17:21:03 -0500

    table exam

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -4,14 +4,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatTableModule } from '@angular/material/table';
 
 
 const material = [
   MatCardModule,
   MatInputModule,
   MatSliderModule,
-  MatButtonModule
+  MatButtonModule,
+  MatTableModule
 ]
 
 @NgModule({
